25094d766d734757671a0e565645f1aa76c40baa
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-10-03 10:42:21 +0300

    main_style: delete msg/clear chat style

--- weather_test.py
+++ weather_test.py
@@ -9,38 +9,47 @@ myToken = os.getenv('myToken')
 myApiWeather = os.getenv('myApiWeather')
 
 
-global lat,lon
+global lat,lon,mes_id,main_mes_id
 
 bot = telebot.TeleBot(myToken)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  bot.send_message(message.chat.id,'Здравствуйте! Введите координаты')
-  bot.send_message(message.chat.id,'Введите широту ==>')
+  global mes_id,main_mes_id
+  # bot.send_message(message.chat.id,'Здравствуйте! Введите координаты')
+  main_mes_id = bot.send_message(message.chat.id,'Здравствуйте! Введите координаты').message_id
+  mes_id = bot.send_message(message.chat.id,'Введите широту ==>').message_id
+  bot.delete_message(message.chat.id, message.id)
   bot.register_next_step_handler(message, send_lat)
 
 
 # ! Создать еще один message_hendler к lat присваивается значение /старт нужно все разделить  
 @bot.message_handler()
 def send_lat(message):
-  global lat,lon
-  bot.send_message(message.chat.id,'Введите долготу ==>')
+  global lat,lon,mes_id,main_mes_id,user_msg
+  bot.delete_message(message.chat.id,message_id=mes_id)
+  bot.delete_message(message.chat.id,message_id=main_mes_id)
+  mes_id = bot.send_message(message.chat.id,'Введите долготу ==>').message_id
   lat = message.text
+  bot.delete_message(message.chat.id, message.id)
   bot.register_next_step_handler(message, send_lon)
+  
 
 def send_lon(message):
   global lat,lon
   lon = message.text
+  bot.delete_message(message.chat.id,message_id=mes_id)
   send_data(message)
+  bot.delete_message(message.chat.id, message.id)
 
 def send_data(message):
-  global lat,lon
+  global lat,lon,user_msg
   # print(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={myApiWeather}&units=metric')
   req = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={myApiWeather}&units=metric')
   # #todo/-/  в конце url для выдачи в градусах ==> &units=metric 
   weather_data = json.loads(req.text)
   temperature = weather_data['list'][0]['main']['temp']
-  bot.send_message(message.chat.id, str(temperature) + ' °C')
+  bot.send_message(message.chat.id,f'Температура по координатам:\n{lat},{lon}\n' + str(temperature) + ' °C')
 
 bot.polling(none_stop=True)
